244c3b23eae1febc3f923a757e93381ff0786d33
henrylee2cn <henrylee2cn@gmail.com>
2018-12-06 21:09:24 +0800

    refactor: refactor all
    Change-Id: If81db423045b03d701e1d93ae935936c30a090a3

--- aster/aster_test.go
+++ aster/aster_test.go
@@ -1,133 +1,138 @@
 package aster_test
 
 import (
+	"fmt"
+	"sort"
+	"strings"
 	"testing"
 
 	"github.com/henrylee2cn/aster/aster"
 )
 
-func TestStruct(t *testing.T) {
-	var src = `package test
-// S comment
-type S struct {
-	// a doc
-	A string` + "`json:\"a\"`" + ` // a comment
-	// bcd doc
-	B,C,D int // line comment
-	E int
-}
-`
-	f, err := aster.ParseFile("../_out/struct1.go", src)
-	if err != nil {
-		t.Fatal(err)
-	}
-	s, ok := f.LookupType("S")
-	if !ok {
-		t.FailNow()
-	}
-	t.Logf("package:%s, filename:%s, typename:%s", s.PkgName(), s.Filename(), s.Name())
-	t.Log(s)
+// func TestStruct(t *testing.T) {
+// 	var src = `package test
+// // S comment
+// type S struct {
+// 	// a doc
+// 	A string` + "`json:\"a\"`" + ` // a comment
+// 	// bcd doc
+// 	B,C,D int // line comment
+// 	E int
+// }
+// `
+// 	f, err := aster.ParseFile("../_out/struct1.go", src)
+// 	if err != nil {
+// 		t.Fatal(err)
+// 	}
+// 	s, ok := f.LookupType("S")
+// 	if !ok {
+// 		t.FailNow()
+// 	}
+// 	t.Logf("package:%s, filename:%s, typename:%s", s.PkgName(), s.Filename(), s.Name())
+// 	t.Log(s)
 
-	// test tag
-	aField, ok := s.FieldByName("A")
-	if !ok {
-		t.FailNow()
-	}
-	aField.Tags.AddOptions("json", "omitempty")
+// 	// test tag
+// 	aField, ok := s.FieldByName("A")
+// 	if !ok {
+// 		t.FailNow()
+// 	}
+// 	aField.Tags.AddOptions("json", "omitempty")
 
-	bField, ok := s.FieldByName("B")
-	if !ok {
-		t.FailNow()
-	}
-	bField.Tags.Set(&aster.Tag{
-		Key:     "json",
-		Name:    "b",
-		Options: []string{"omitempty"},
-	})
+// 	bField, ok := s.FieldByName("B")
+// 	if !ok {
+// 		t.FailNow()
+// 	}
+// 	bField.Tags.Set(&aster.Tag{
+// 		Key:     "json",
+// 		Name:    "b",
+// 		Options: []string{"omitempty"},
+// 	})
 
-	dField, ok := s.FieldByName("D")
-	if !ok {
-		t.FailNow()
-	}
-	dField.Tags.Set(&aster.Tag{
-		Key:     "json",
-		Name:    "d",
-		Options: []string{"omitempty"},
-	})
+// 	dField, ok := s.FieldByName("D")
+// 	if !ok {
+// 		t.FailNow()
+// 	}
+// 	dField.Tags.Set(&aster.Tag{
+// 		Key:     "json",
+// 		Name:    "d",
+// 		Options: []string{"omitempty"},
+// 	})
 
-	eField, ok := s.FieldByName("E")
-	if !ok {
-		t.FailNow()
-	}
-	eField.Tags.Set(&aster.Tag{
-		Key:  "json",
-		Name: "e",
-	})
+// 	eField, ok := s.FieldByName("E")
+// 	if !ok {
+// 		t.FailNow()
+// 	}
+// 	eField.Tags.Set(&aster.Tag{
+// 		Key:  "json",
+// 		Name: "e",
+// 	})
 
-	ret, err := f.Format()
-	if err != nil {
-		t.Fatal(err)
-	}
-	t.Log(ret)
+// 	ret, err := f.Format()
+// 	if err != nil {
+// 		t.Fatal(err)
+// 	}
+// 	t.Log(ret)
 
-	err = f.Store()
-	if err != nil {
-		t.Fatal(err)
-	}
-}
+// 	err = f.Store()
+// 	if err != nil {
+// 		t.Fatal(err)
+// 	}
+// }
 
-func TestAlias(t *testing.T) {
-	var src = `package test
-	// A comment
-	type A int
-	// B comment
-	type B *int
-	// C comment
-	type C []*int
-	// D comment
-	type D map[string]*int
-	// E comment
-	type E chan<- int
-	// F comment
-	type F = struct{}
-	// G comment
-	type G = *struct{}
-`
-	f, err := aster.ParseFile("../_out/alias1.go", src)
-	if err != nil {
-		t.Fatal(err)
-	}
-	f.Inspect(func(obj aster.Object) bool {
-		t.Log(obj.ObjKind(), obj.Kind(), obj)
-		return true
-	})
-}
+// func TestAlias(t *testing.T) {
+// 	var src = `package test
+// 	// A comment
+// 	type A int
+// 	// B comment
+// 	type B *int
+// 	// C comment
+// 	type C []*int
+// 	// D comment
+// 	type D map[string]*int
+// 	// E comment
+// 	type E chan<- int
+// 	// F comment
+// 	type F = struct{}
+// 	// G comment
+// 	type G = *struct{}
+// `
+// 	f, err := aster.ParseFile("../_out/alias1.go", src)
+// 	if err != nil {
+// 		t.Fatal(err)
+// 	}
+// 	f.Inspect(func(obj aster.Object) bool {
+// 		t.Log(obj.ObjKind(), obj.Kind(), obj)
+// 		return true
+// 	})
+// }
 
-func TestFunc(t *testing.T) {
-	var src = `package test
-	// S comment
-	type S struct {}
-	// String comment
-	func(s *S)String()string {return ""}
-	// F1 comment
-	func F1(i int){}
-	// F2 FuncLit!
-	var F2=func()int{}
-`
-	f, err := aster.ParseFile("../_out/func1.go", src)
-	if err != nil {
-		t.Fatal(err)
-	}
-	f.Inspect(func(n aster.Object) bool {
-		if n.Kind() == aster.Func {
-			t.Log(n.String())
-		}
-		return true
-	})
-}
+// func TestFunc(t *testing.T) {
+// 	var src = `package test
+// 	// S comment
+// 	type S struct {}
+// 	// String comment
+// 	func(s *S)String()string {return ""}
+// 	// F1 comment
+// 	func F1(i int){}
+// 	// F2 FuncLit!
+// 	var F2=func()int{}
+// `
+// 	f, err := aster.ParseFile("../_out/func1.go", src)
+// 	if err != nil {
+// 		t.Fatal(err)
+// 	}
+// 	f.Inspect(func(n aster.Object) bool {
+// 		if n.Kind() == aster.Func {
+// 			t.Log(n.String())
+// 		}
+// 		return true
+// 	})
+// }
 
 func TestScope(t *testing.T) {
 	var src = `package test
+	// import "fmt"
+	// var X=fmt.Print
 	// S comment
 	type S int
 	// String comment
@@ -150,13 +155,46 @@ func TestScope(t *testing.T) {
 		S2
 	)
 `
-	f, err := aster.ParseFile("../_out/func1.go", src)
-	if err != nil {
-		t.Fatal(err)
-	}
+	prog := aster.NewLoader().MustAddFile("../_out/func1.go", src).MustLoad()
+	printProgram(prog)
+	pkgInfo := prog.Package("test")
+	t.Logf("%#v", pkgInfo.Info)
+}
 
-	f.Inspect(func(obj aster.Object) bool {
-		t.Logf("ObjKind:%s, Kind:%s, IsGlobal:%v, Preview:%s", obj.ObjKind(), obj.Kind(), obj.IsGlobal(), obj)
-		return true
-	})
+func printProgram(prog *aster.Program) {
+	// Created packages are the initial packages specified by a call
+	// to CreateFromFilenames or CreateFromFiles.
+	var names []string
+	for _, info := range prog.Created {
+		names = append(names, info.Pkg.Path())
+	}
+	fmt.Printf("created: %s\n", names)
+
+	// Imported packages are the initial packages specified by a
+	// call to Import or ImportWithTests.
+	names = nil
+	for _, info := range prog.Imported {
+		if strings.Contains(info.Pkg.Path(), "internal") {
+			continue // skip, to reduce fragility
+		}
+		names = append(names, info.Pkg.Path())
+	}
+	sort.Strings(names)
+	fmt.Printf("imported: %s\n", names)
+
+	// InitialPackages contains the union of created and imported.
+	names = nil
+	for _, info := range prog.InitialPackages() {
+		names = append(names, info.Pkg.Path())
+	}
+	sort.Strings(names)
+	fmt.Printf("initial: %s\n", names)
+
+	// AllPackages contains all initial packages and their dependencies.
+	names = nil
+	for pkg := range prog.AllPackages {
+		names = append(names, pkg.Path())
+	}
+	sort.Strings(names)
+	fmt.Printf("all: %s\n", names)
 }
---
+++ aster/loader.go
@@ -0,0 +1,244 @@
+// Copyright 2018 henrylee2cn. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package aster
+
+import (
+	"fmt"
+	"go/ast"
+	"go/parser"
+	"go/token"
+	"go/types"
+
+	"golang.org/x/tools/go/loader"
+)
+
+// A Program is a Go program loaded from source.
+type Program struct {
+	// Fset the file set for this program
+	Fset *token.FileSet
+
+	// Created[i] contains the initial package whose ASTs or
+	// filenames were supplied by AddFiles() and MustAddFiles(), followed by
+	// the external test package, if any, of each package in
+	// Imports() and ImportWithTests() ordered by ImportPath.
+	//
+	// NOTE: these files must not import "C".  Cgo preprocessing is
+	// only performed on imported packages, not ad hoc packages.
+	//
+	// TODO(adonovan): we need to copy and adapt the logic of
+	// goFilesPackage (from $GOROOT/src/cmd/go/build.go) and make
+	// Config.Import and Config.Create methods return the same kind
+	// of entity, essentially a build.Package.
+	// Perhaps we can even reuse that type directly.
+	Created []*PackageInfo
+
+	// Imported contains the initially imported packages,
+	// as specified by Imports() and ImportWithTests().
+	Imported map[string]*PackageInfo
+
+	// AllPackages contains the PackageInfo of every package
+	// encountered by Load: all initial packages and all
+	// dependencies, including incomplete ones.
+	AllPackages map[*types.Package]*PackageInfo
+}
+
+// PackageInfo holds the ASTs and facts derived by the type-checker
+// for a single package.
+//
+// Not mutated once exposed via the API.
+//
+type PackageInfo struct {
+	Pkg                   *types.Package
+	Importable            bool        // true if 'import "Pkg.Path()"' would resolve to this
+	TransitivelyErrorFree bool        // true if Pkg and all its dependencies are free of errors
+	Files                 []*ast.File // syntax trees for the package's files
+	Errors                []error     // non-nil if the package had errors
+	types.Info                        // type-checker deductions.
+}
+
+// Loader loading a whole program from Go source code.
+type Loader struct {
+	conf loader.Config
+}
+
+// NewLoader creates a loader.
+func NewLoader(mode ...parser.Mode) *Loader {
+	loader := new(Loader)
+	loader.conf.ParserMode = parser.ParseComments
+	loader.conf.AllowErrors = true
+	for _, m := range mode {
+		loader.conf.ParserMode |= m
+	}
+	return loader
+}
+
+// AddFile parses the source code of a single Go source file.
+//
+// src specifies the parser input as a string, []byte, or io.Reader, and
+// filename is its apparent name.  If src is nil, the contents of
+// filename are read from the file system.
+//
+func (l *Loader) AddFile(filename string, src interface{}) error {
+	f, err := l.conf.ParseFile(filename, src)
+	if err != nil {
+		return err
+	}
+	l.conf.CreateFromFiles("", f)
+	return nil
+}
+
+// MustAddFile is similar to AddFile, but panic when existing error.
+func (l *Loader) MustAddFile(filename string, src interface{}) *Loader {
+	err := l.AddFile(filename, src)
+	if err != nil {
+		panic(err)
+	}
+	return l
+}
+
+// Imports imports packages that will be imported from source,
+// the set of initial source packages located relative to $GOPATH.
+func (l *Loader) Imports(pkgPath ...string) *Loader {
+	for _, p := range pkgPath {
+		l.conf.Import(p)
+	}
+	return l
+}
+
+// ImportWithTests imports packages that will be imported from source,
+// the set of initial source packages located relative to $GOPATH.
+// The package will be augmented by any *_test.go files in
+// its directory that contain a "package x" (not "package x_test")
+// declaration.
+//
+// In addition, if any *_test.go files contain a "package x_test"
+// declaration, an additional package comprising just those files will
+// be added to CreatePkgs.
+//
+func (l *Loader) ImportWithTests(pkgPath ...string) *Loader {
+	for _, p := range pkgPath {
+		l.conf.ImportWithTests(p)
+	}
+	return l
+}
+
+// Load creates the initial packages specified by conf.{Create,Import}Pkgs,
+// loading their dependencies packages as needed.
+//
+// On success, Load returns a Program containing a PackageInfo for
+// each package.  On failure, it returns an error.
+//
+// If AllowErrors is true, Load will return a Program even if some
+// packages contained I/O, parser or type errors, or if dependencies
+// were missing.  (Such errors are accessible via PackageInfo.Errors.  If
+// false, Load will fail if any package had an error.
+//
+// It is an error if no packages were loaded.
+//
+func (l *Loader) Load() (prog *Program, err error) {
+	defer func() {
+		if p := recover(); p != nil {
+			err = fmt.Errorf("%v", p)
+		}
+	}()
+	p, err := l.conf.Load()
+	if err != nil {
+		return nil, err
+	}
+	return newProgram(p), nil
+}
+
+// MustLoad is similar to Load, but panic when existing error.
+func (l *Loader) MustLoad() *Program {
+	p, err := l.Load()
+	if err != nil {
+		panic(err)
+	}
+	return p
+}
+
+// newProgram creates a program object.
+func newProgram(prog *loader.Program) *Program {
+	p := &Program{
+		Fset:        prog.Fset,
+		Imported:    make(map[string]*PackageInfo, len(prog.Imported)),
+		AllPackages: make(map[*types.Package]*PackageInfo, len(prog.AllPackages)),
+	}
+	var solved = make(map[*loader.PackageInfo]*PackageInfo, len(prog.AllPackages))
+	for _, pkg := range prog.Created {
+		newPkg := newPackageInfo(pkg)
+		solved[pkg] = newPkg
+		p.Created = append(p.Created, newPkg)
+	}
+	for k, pkg := range prog.Imported {
+		newPkg := newPackageInfo(pkg)
+		solved[pkg] = newPkg
+		p.Imported[k] = newPkg
+	}
+	for k, pkg := range prog.AllPackages {
+		if newPkg, ok := solved[pkg]; ok {
+			p.AllPackages[k] = newPkg
+		} else {
+			newPkg := newPackageInfo(pkg)
+			solved[pkg] = newPkg
+			p.AllPackages[k] = newPkg
+		}
+	}
+	return p
+}
+
+// newPackageInfo creates a package info.
+func newPackageInfo(pkg *loader.PackageInfo) *PackageInfo {
+	return &PackageInfo{
+		Pkg:                   pkg.Pkg,
+		Importable:            pkg.Importable,
+		TransitivelyErrorFree: pkg.TransitivelyErrorFree,
+		Files:                 pkg.Files,
+		Errors:                pkg.Errors,
+		Info:                  pkg.Info,
+	}
+}
+
+// Package returns the ASTs and results of type checking for the
+// specified package.
+// NOTE: return nil, if the package does not exist.
+func (p *Program) Package(path string) *PackageInfo {
+	for k, v := range p.AllPackages {
+		if k.Path() == path {
+			return v
+		}
+	}
+	for _, info := range p.Created {
+		if path == info.Pkg.Path() {
+			return info
+		}
+	}
+	return nil
+}
+
+func (p *PackageInfo) String() string {
+	return p.Pkg.Path()
+}
+
+// InitialPackages returns a new slice containing the set of initial
+// packages (Created + Imported) in unspecified order.
+func (p *Program) InitialPackages() []*PackageInfo {
+	infos := make([]*PackageInfo, 0, len(p.Created)+len(p.Imported))
+	infos = append(infos, p.Created...)
+	for _, info := range p.Imported {
+		infos = append(infos, info)
+	}
+	return infos
+}
